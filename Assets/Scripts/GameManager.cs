20111090279f569d57fb6f5ac061a3d020008976
Carlos Marques <catralitos11@gmail.com>
2021-04-20 21:23:24 +0100

    Fiz forma de alternar entre duas resoluçoes no game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,27 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-        
+    public bool fullscreen;
+
+    private void Awake()
+    {
+        if (fullscreen) Screen.SetResolution(1920, 1080, true, 60);
+        else Screen.SetResolution(1280, 720, false, 60);
+    }
+
+    public void ToggleFullscreen()
+    {
+        if (fullscreen)
+        {
+            Screen.SetResolution(1280, 720, false, 60);
+            fullscreen = false;
+        }
+        else
+        {
+            Screen.SetResolution(1920, 1080, true, 60);
+            fullscreen = true;
+        }
+    }
 }
