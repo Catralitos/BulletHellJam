8b516fd1819426988b0b0a44fc6efd1e39810b4a
Carlos Marques <catralitos11@gmail.com>
2021-04-22 18:33:22 +0100

    Title screen feito, mas falta arte da character e testar fullscreen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,42 @@
 using System;
+using Audio;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public bool fullscreen;
+    public bool mouseControls = true;
+
+    private AudioManager _audioManager;
+
+    #region SingleTon
+
+    public static GameManager Instance { get; private set; }
 
     private void Awake()
     {
-        if (fullscreen) Screen.SetResolution(1920, 1080, true, 60);
-        else Screen.SetResolution(1280, 720, false, 60);
+        // Needed if we want the audio manager to persist through scenes
+        if (Instance == null)
+        {
+            Instance = this;
+            _audioManager = AudioManager.Instance;
+            if (fullscreen) Screen.SetResolution(1920, 1080, true, 60);
+            else Screen.SetResolution(1280, 720, false, 60);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        
     }
 
+    #endregion
+    
     public void ToggleFullscreen()
     {
         if (fullscreen)
---
+++ Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,51 @@
+using Audio;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleScreenManager : MonoBehaviour
+{
+
+    public Button mouseButton, controllerButton, fullscreenButton;
+
+    private GameManager _gameManager;
+    private AudioManager _audioManager;
+    
+    private void Start()
+    {
+        _audioManager = AudioManager.Instance;
+        _gameManager = GameManager.Instance;
+        mouseButton.onClick.AddListener(StartMouseGame);
+        controllerButton.onClick.AddListener(StartControllerGame);
+        fullscreenButton.onClick.AddListener(ToggleFullscreen);
+
+        //audioManager.Play("TitleScreen");
+    }
+
+    private void ToggleFullscreen()
+    {
+        if (_gameManager.fullscreen)
+        {
+            Screen.SetResolution(1280, 720, false, 60);
+            _gameManager.fullscreen = false;
+        }
+        else
+        {
+            Screen.SetResolution(1920, 1080, true, 60);
+            _gameManager.fullscreen = true;
+        }
+    }
+
+    private void StartMouseGame()
+    {
+        _gameManager.mouseControls = true;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void StartControllerGame()
+    {
+        _gameManager.mouseControls = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+ 
+}
