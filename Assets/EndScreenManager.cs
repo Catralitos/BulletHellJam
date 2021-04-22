4ea01c2a5e065bb3e80cbcc9450c367e7e9bb2ed
Carlos Marques <catralitos11@gmail.com>
2021-04-22 19:27:38 +0100

    Creditos feitos

---
+++ Assets/EndScreenManager.cs
@@ -0,0 +1,31 @@
+using Audio;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndScreenManager : MonoBehaviour
+{
+    public Button titleButton, exitButton;
+
+    private GameManager _gameManager;
+    private AudioManager _audioManager;
+    
+    private void Start()
+    {
+        _audioManager = AudioManager.Instance;
+        _gameManager = GameManager.Instance;
+        titleButton.onClick.AddListener(ToTitle);
+        exitButton.onClick.AddListener(ExitGame);
+        _audioManager.Play("TitleScreen");
+    }
+
+    private void ToTitle()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+}
