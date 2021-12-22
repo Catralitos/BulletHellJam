d59b063baf0e94babbf50bafc96e0af16672cb16
Carlos Marques <catralitos11@gmail.com>
2021-12-22 16:58:28 +0000

    Meti hits nos inimigos

--- Assets/Scripts/Enemies/Base/EnemyBase.cs
+++ Assets/Scripts/Enemies/Base/EnemyBase.cs
@@ -17,12 +17,25 @@ namespace Enemies.Base
         public int maxHealth;
         public float randomDropChance = 0.1f;
         public List<GameObject> powerUps;
-        
+
+        public bool hasInvincibility = true;
+
+        private SpriteRenderer _renderer;
+        private Material _defaultMaterial;
+        public Material hitMaterial;
+        public float invincibilityTime;
+        private bool _invincible;
+
         public GameObject explosionPrefab;
 
         protected virtual void Start()
         {
             currentHealth = maxHealth;
+            if (hasInvincibility)
+            {
+                _renderer = GetComponent<SpriteRenderer>();
+                _defaultMaterial = _renderer.material;
+            }
         }
 
         public void Call(string messageName)
@@ -32,9 +45,31 @@ namespace Enemies.Base
 
         protected virtual void Hit(int damage)
         {
+            if (_invincible) return;
+            Debug.Log("Passou o _invincible");
             if (!IsAlive) return;
             currentHealth = Mathf.Max(currentHealth - damage, 0);
-            if (!IsAlive) Die();
+            Debug.Log("Subtraiu vida");
+            if (!IsAlive)
+            {
+                Die();
+            }
+            else
+            {
+                if (hasInvincibility)
+                {
+                    Debug.Log("Entrou no hasInvincibility");
+                    _renderer.material = hitMaterial;
+                    _invincible = true;
+                    Invoke(nameof(RestoreVulnerability), invincibilityTime);
+                }
+            }
+        }
+
+        private void RestoreVulnerability()
+        {
+            _renderer.material = _defaultMaterial;
+            _invincible = false;
         }
 
         protected virtual void Die()
@@ -71,10 +106,12 @@ namespace Enemies.Base
 
         protected override void Hit(int damage)
         {
+            base.Hit(damage);
+            /*Debug.Log("Entrou no hit");
             if (!IsAlive) return;
             currentHealth = Mathf.Max(currentHealth - damage, 0);
             State.OnGetHit();
-            if (!IsAlive) Die();
+            if (!IsAlive) Die();*/
         }
 
         protected virtual void Update()
